02e74a777daadc5c7e334f75845afb58abd00d8f
Ruben D. Garcia <rdgp99@gmail.com>
2025-10-10 01:21:49 -0500

    add worker comments

--- worker.go
+++ worker.go
@@ -14,13 +14,26 @@ type Args[T any] struct {
 	Drainer *Drain[T]
 }
 
+// Promise is a function that takes a generic type T and returns an error
 type Promise[T any] func(arg Args[T]) error
 
+// Worker is an interface that wraps an async function with any type of parameter
 type Worker interface {
+	Executable
+	Retryable
+}
+
+// Executable is an interface that wraps an executable functionality
+type Executable interface {
 	Execute() error
+}
+
+// Retryable is an interfacee that wraps a retriable functionality
+type Retryable interface {
 	WithRetry(attempts uint, sleep time.Duration) Worker
 }
 
+// Task is a Worker wrapper around Promise to conform to the Worker interface
 type Task[T any] struct {
 	fn       Promise[T]
 	arg      Args[T]
@@ -28,10 +41,12 @@ type Task[T any] struct {
 	sleep    time.Duration
 }
 
+// NewTask creates a new Task of type T
 func NewTask[T any](fn Promise[T]) *Task[T] {
 	return &Task[T]{fn: fn, attempts: 1}
 }
 
+// Execute runs the Promise with the provided parameter
 func (t *Task[T]) Execute() error {
 	currentSleep := t.sleep
 	for i := uint(0); i < t.attempts; i++ {
@@ -52,17 +67,20 @@ func (t *Task[T]) Execute() error {
 	return nil
 }
 
+// WithRetry assigns retry config to the worker underlying func
 func (t *Task[T]) WithRetry(attempts uint, sleep time.Duration) Worker {
 	t.attempts = attempts
 	t.sleep = sleep
 	return t
 }
 
+// DrainTo defines Drainer output of the current Task
 func (t *Task[T]) DrainTo(d *Drain[T]) *Task[T] {
 	t.arg.Drainer = d
 	return t
 }
 
+// WithInput defines input of the current Task
 func (t *Task[T]) WithInput(input *T) *Task[T] {
 	t.arg.Input = input
 	return t

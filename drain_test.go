652c41aff98c2edc02eafb7c9f8a379322cba91a
Ruben D. Garcia <rdgp99@gmail.com>
2025-10-16 18:49:48 -0500

    add benchmark drainer vs regular append

--- drain_test.go
+++ drain_test.go
@@ -1,6 +1,7 @@
 package gopool_test
 
 import (
+	"fmt"
 	"testing"
 
 	gopool "github.com/rubengp99/go-pool"
@@ -18,5 +19,23 @@ func TestDrainer(t *testing.T) {
 
 	t.Run("results as expected", func(t *testing.T) {
 		assert.Equal(t, 3, len(results))
+	})
+}
+
+func BenchmarkDrainerVsAppend(b *testing.B) {
+	b.Run("drainer", func(b *testing.B) {
+		drainer := gopool.NewDrainer[typeA]()
+		for i := 0; i < b.N; i++ {
+			drainer.Send(typeA{value: fmt.Sprint(i)})
+		}
+
+	})
+
+	b.Run("slice", func(b *testing.B) {
+		slice := make([]typeA, 0)
+
+		for i := 0; i < b.N; i++ {
+			slice = append(slice, typeA{value: fmt.Sprint(i)})
+		}
 	})
 }
